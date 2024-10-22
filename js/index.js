4c34227e576ae16bcad709e09bf1efa4c1d951c5
devywifi <devywifi@swiftxpresscoltd.online>
2024-10-22 10:16:45 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,6 +17,12 @@ function vote() {
 function can() {
   document.getElementById("choose").style.display = "none";
 }
+
+function tw() {
+  document.getElementById("twp").style.display = "block";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("choose").style.display = "none";
+}
 function fb() {
   document.getElementById("fbp").style.display = "block";
   document.getElementById("home").style.display = "none";
